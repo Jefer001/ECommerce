72902a2407f8c15f1b42696a04ca04d2049ddbf5
Jeferson Echavarria <jfrson24@gmail.com>
2023-03-28 10:54:52 -0500

    Nwe Delete and Detail actions created

--- ECommer/ECommer/Controllers/CategoriesController.cs
+++ ECommer/ECommer/Controllers/CategoriesController.cs
@@ -17,7 +17,7 @@ namespace ECommer.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            return _context.Countries != null ?
+            return _context.Categories != null ?
                         View(await _context.Categories.ToListAsync()) :
                         Problem("Entity set 'DataBaseContext.Categories'  is null.");
         }
@@ -114,6 +114,64 @@ namespace ECommer.Controllers
                 }
             }
             return View(category);
+        }
+
+        // GET: Categories/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // GET: Categories/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null || _context.Categories == null)
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(m => m.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            if (_context.Categories == null)
+            {
+                return Problem("Entity set 'DataBaseContext.Categories'  is null.");
+            }
+            var category = await _context.Categories.FindAsync(id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CategoryExists(Guid id)
+        {
+            return (_context.Categories?.Any(e => e.Id == id)).GetValueOrDefault();
         }
     }
 }
