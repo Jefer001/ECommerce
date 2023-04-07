a240b9d49401b9ad553f7dec7f36ad13e28329f0
jefferson echavarria <jfrson__@live.com>
2023-04-06 20:43:29 -0500

    New addCity Get & Post actions created

--- ECommer/ECommer/Controllers/CountriesController.cs
+++ ECommer/ECommer/Controllers/CountriesController.cs
@@ -8,15 +8,18 @@ namespace ECommer.Controllers
 {
     public class CountriesController : Controller
     {
-        private readonly DataBaseContext _context;
+		#region Builder
+		private readonly DataBaseContext _context;
 
         public CountriesController(DataBaseContext context)
         {
             _context = context;
         }
+		#endregion
 
-        // GET: Countries
-        public async Task<IActionResult> Index()
+		#region Country actions
+		// GET: Countries
+		public async Task<IActionResult> Index()
         {
             return _context.Countries != null ?
                         View(await _context.Countries
@@ -145,8 +148,10 @@ namespace ECommer.Controllers
 
             return RedirectToAction(nameof(Index));
         }
+		#endregion
 
-        [HttpGet]
+		#region State actions
+		[HttpGet]
         public async Task<IActionResult> AddState(Guid? countryId)
         {
 			if (countryId == null) return NotFound();
@@ -173,8 +178,8 @@ namespace ECommer.Controllers
                 {
                     State state = new()
                     {
-                       // Cities = new List<City>(),
-                        Country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == stateViewModel.CountryId),
+						Cities = new List<City>(),
+						Country = await _context.Countries.FirstOrDefaultAsync(c => c.Id == stateViewModel.CountryId),
 					    Name = stateViewModel.Name,
                         CreatedDate = DateTime.Now,
                         ModifiedDate = null
@@ -302,5 +307,164 @@ namespace ECommer.Controllers
 
 			return RedirectToAction(nameof(Details), new {id = state.Country.Id});
 		}
+		#endregion
+
+		#region City actions
+		[HttpGet]
+		public async Task<IActionResult> AddCity(Guid? stateId)
+		{
+			if (stateId == null) return NotFound();
+
+			State state = await _context.States
+				.FirstOrDefaultAsync(c => c.Id == stateId);
+
+			if (stateId == null) return NotFound();
+
+			CityViewModel cityViewModel = new()
+			{
+				 StateId = state.Id
+			};
+			return View(cityViewModel);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> AddCity(CityViewModel cityViewModel)
+		{
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					City city = new()
+					{
+						State = await _context.States.FirstOrDefaultAsync(c => c.Id == cityViewModel.StateId),
+						Name = cityViewModel.Name,
+						CreatedDate = DateTime.Now,
+						ModifiedDate = null
+					};
+
+					_context.Add(city);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(DetailsState), new { stateId = cityViewModel.StateId });
+				}
+				catch (DbUpdateException dbUpdateException)
+				{
+					if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+						ModelState.AddModelError(string.Empty, "Ya existe una ciudad con el mismo nombre en este dpto/estado");
+					else
+						ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+				}
+				catch (Exception ex)
+				{
+					ModelState.AddModelError(string.Empty, ex.Message);
+				}
+			}
+			return View(cityViewModel);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> EditCity(Guid? stateId)
+		{
+			if (stateId == null || _context.States == null) return NotFound();
+
+			State state = await _context.States
+				.Include(c => c.Country)
+				.FirstOrDefaultAsync(s => s.Id == stateId);
+
+			if (state == null) return NotFound();
+
+			StateViewModel stateViewModel = new()
+			{
+				CountryId = state.Country.Id,
+				Id = state.Id,
+				Name = state.Name,
+				CreatedDate = state.CreatedDate
+			};
+			return View(stateViewModel);
+		}
+
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> EditCity(Guid countryId, StateViewModel stateViewModel)
+		{
+			if (countryId != stateViewModel.CountryId) return NotFound();
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					State state = new()
+					{
+						Id = stateViewModel.Id,
+						Name = stateViewModel.Name,
+						CreatedDate = stateViewModel.CreatedDate,
+						ModifiedDate = DateTime.Now
+					};
+					_context.Update(state);
+					await _context.SaveChangesAsync();
+					return RedirectToAction(nameof(Index), new { Id = stateViewModel.CountryId });
+				}
+				catch (DbUpdateException dbUpdateException)
+				{
+					if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+						ModelState.AddModelError(string.Empty, "Ya existe un estado con el mismo nombre.");
+					else
+						ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+				}
+				catch (Exception ex)
+				{
+					ModelState.AddModelError(string.Empty, ex.Message);
+				}
+			}
+			return View(stateViewModel);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> DetailsCity(Guid? stateId)
+		{
+			if (stateId == null || _context.States == null) return NotFound();
+
+			var state = await _context.States
+				.Include(c => c.Country)
+				.Include(c => c.Cities)
+				.FirstOrDefaultAsync(s => s.Id == stateId);
+
+			if (state == null) return NotFound();
+
+			return View(state);
+		}
+
+		public async Task<IActionResult> DeleteCity(Guid? stateId)
+		{
+			if (stateId == null || _context.Countries == null) return NotFound();
+
+			var state = await _context.States
+				.Include(c => c.Country)
+				.Include(c => c.Cities)
+				.FirstOrDefaultAsync(c => c.Id == stateId);
+
+			if (state == null) return NotFound();
+
+			return View(state);
+		}
+
+		[HttpPost, ActionName("DeleteCity")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteCityConfirmed(Guid stateId)
+		{
+			if (_context.States == null) return Problem("Entity set 'DataBaseContext.State'  is null.");
+
+			var state = await _context.States
+				.Include(c => c.Country)
+				.Include(c => c.Cities)
+				.FirstOrDefaultAsync(c => c.Id == stateId);
+
+			if (state != null) _context.States.Remove(state);
+
+			await _context.SaveChangesAsync();
+
+			return RedirectToAction(nameof(Details), new { id = state.Country.Id });
+		}
+		#endregion
 	}
 }
---
+++ ECommer/ECommer/Models/CityViewModel.cs
@@ -0,0 +1,11 @@
+﻿using ECommer.DAL.Entities;
+
+namespace ECommer.Models
+{
+	public class CityViewModel : City
+	{
+		#region Properties
+		public Guid StateId { get; set; }
+		#endregion
+	}
+}
