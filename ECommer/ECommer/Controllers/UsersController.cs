a334ef190346bb987ccbd91ba4d893b36fc6b967
jefferson echavarria <jfrson__@live.com>
2023-05-07 09:52:25 -0500

    CreatedAdmin action developed

--- ECommer/ECommer/Controllers/UsersController.cs
+++ ECommer/ECommer/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
 ﻿using ECommer.DAL;
+using ECommer.DAL.Entities;
+using ECommer.Helpers;
+using ECommer.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,12 +12,18 @@ namespace ECommer.Controllers
     {
         #region Constants
         private readonly DataBaseContext _context;
+        private readonly IUserHelpers _userHelpers;
+        private readonly IDropDownListHelper _dropDownListHelper;
+        private readonly IAzureBlobHelper _azureBlobHelper;
         #endregion
 
         #region Builder
-        public UsersController(DataBaseContext context)
+        public UsersController(DataBaseContext context, IUserHelpers userHelpers, IDropDownListHelper dropDownListHelper, IAzureBlobHelper azureBlobHelper)
         {
             _context = context;
+            _userHelpers = userHelpers;
+            _dropDownListHelper = dropDownListHelper;
+            _azureBlobHelper = azureBlobHelper;
         }
         #endregion
 
@@ -29,6 +38,80 @@ namespace ECommer.Controllers
                        .ThenInclude(s => s.Country)
                        .ToListAsync()) :
                        Problem("Entity set 'DataBaseContext.Users'  is null.");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateAdmin()
+        {
+            AddUserViewModel addUserViewModel = new()
+            {
+                Id = Guid.Empty,
+                Countries = await _dropDownListHelper.GetDDLCountriesAsync(),
+                States = await _dropDownListHelper.GetDDLStatesAsync(new Guid()),
+                Cities = await _dropDownListHelper.GetDDLCitiesAsync(new Guid()),
+                UserType = Enum.UserType.Admin
+            };
+            return View(addUserViewModel);  
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAdmin(AddUserViewModel addUserViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+
+                if (addUserViewModel.ImageFile != null)
+                    imageId = await _azureBlobHelper.UploadAzureBlobAsync(addUserViewModel.ImageFile, "users");
+
+                addUserViewModel.ImageId = imageId;
+                addUserViewModel.CreatedDate = DateTime.Now;
+
+                User user = await _userHelpers.AddUserAsync(addUserViewModel);
+                if (user == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                    await FillDropDownListLocation(addUserViewModel);
+                    return View(addUserViewModel);
+                }
+                return RedirectToAction("Index", "Users");
+            }
+            await FillDropDownListLocation(addUserViewModel);
+            return View(addUserViewModel);
+        }
+
+        [HttpGet]
+        public JsonResult GetStates(Guid countryId)
+        {
+            Country country = _context.Countries
+                .Include(c => c.States)
+                .FirstOrDefault(c => c.Id.Equals(countryId));
+
+            if (country == null) return null;
+
+            return Json(country.States.OrderBy(c => c.Name));
+        }
+
+        [HttpGet]
+        public JsonResult GetCities(Guid stateId)
+        {
+            State state = _context.States
+                .Include(s => s.Cities)
+                .FirstOrDefault(s => s.Id.Equals(stateId));
+
+            if (state == null) return null;
+
+            return Json(state.Cities.OrderBy(s => s.Name));
+        }
+        #endregion
+
+        #region Private methods
+        private async Task FillDropDownListLocation(AddUserViewModel addUserViewModel)
+        {
+            addUserViewModel.Countries = await _dropDownListHelper.GetDDLCountriesAsync();
+            addUserViewModel.States = await _dropDownListHelper.GetDDLStatesAsync(addUserViewModel.CountryId);
+            addUserViewModel.Cities = await _dropDownListHelper.GetDDLCitiesAsync(addUserViewModel.StateId);
         }
         #endregion
     }
