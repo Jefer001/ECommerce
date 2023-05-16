8c999aff9c9fc67b2e8b321f64a13b00c7d8a25b
jefferson echavarria <jfrson__@live.com>
2023-05-16 10:35:13 -0500

    Index product action created

---
+++ ECommer/ECommer/Controllers/ProductsController.cs
@@ -0,0 +1,38 @@
+﻿using ECommer.DAL;
+using ECommer.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommer.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class ProductsController : Controller
+    {
+        #region Constants
+        private readonly DataBaseContext _context;
+        private readonly IAzureBlobHelper _azureBlobHelper;
+        private readonly IDropDownListHelper _dropDownListHelper;
+        #endregion
+
+        #region Builder
+        public ProductsController(DataBaseContext context, IAzureBlobHelper azureBlobHelper, IDropDownListHelper dropDownListHelper)
+        {
+            _context = context;
+            _azureBlobHelper = azureBlobHelper;
+            _dropDownListHelper = dropDownListHelper;
+        }
+        #endregion
+
+        #region Product actions
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Products
+                .Include(p => p.ProductImages)
+                .Include(p => p.ProductCategories)
+                .ThenInclude(pc => pc.Category)
+                .ToListAsync());
+        }
+        #endregion
+    }
+}
