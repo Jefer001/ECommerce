9039a0a09e664d603df3e8e89609da2d135286bf
jefferson echavarria <jfrson__@live.com>
2023-05-16 11:16:21 -0500

    Create GET & POST actions developed

--- ECommer/ECommer/Controllers/ProductsController.cs
+++ ECommer/ECommer/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
 ﻿using ECommer.DAL;
+using ECommer.DAL.Entities;
 using ECommer.Helpers;
+using ECommer.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -32,6 +34,80 @@ namespace ECommer.Controllers
                 .Include(p => p.ProductCategories)
                 .ThenInclude(pc => pc.Category)
                 .ToListAsync());
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            AddProductViewModel addProductViewModel = new()
+            {
+                Categories = await _dropDownListHelper.GetDDLCategoriesAsync(),
+            };
+
+            return View(addProductViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(AddProductViewModel addProductViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    Guid imageId = Guid.Empty;
+                    if (addProductViewModel.ImageFile != null)
+                        imageId = await _azureBlobHelper.UploadAzureBlobAsync(addProductViewModel.ImageFile, "products");
+
+                    Product product = new()
+                    {
+                        Description = addProductViewModel.Description,
+                        Name = addProductViewModel.Name,
+                        Price = addProductViewModel.Price,
+                        Stock = addProductViewModel.Stock,
+                        CreatedDate = DateTime.Now,
+                        ProductCategories = new List<ProductCategory>()
+                    {
+                        new ProductCategory
+                        {
+                            Category = await _context.Categories.FindAsync(addProductViewModel.CategoryId)
+                        }
+                    }
+                    };
+
+                    if (imageId != Guid.Empty)
+                    {
+                        product.ProductImages = new List<ProductImage>()
+                        {
+                            new ProductImage {
+                                ImageId = imageId,
+                                CreatedDate = DateTime.Now,
+                            }
+                        };
+                    }
+
+                    _context.Add(product);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un producto con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            addProductViewModel.Categories = await _dropDownListHelper.GetDDLCategoriesAsync();
+            return View(addProductViewModel);
         }
         #endregion
     }
---
+++ ECommer/ECommer/Models/AddProductViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ECommer.Models
+{
+    public class AddProductViewModel : EditProductViewModel
+    {
+        #region Properties
+        [Display(Name = "Categoría")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public Guid CategoryId { get; set; }
+
+        public IEnumerable<SelectListItem> Categories { get; set; }
+
+        [Display(Name = "Foto")]
+        public IFormFile? ImageFile { get; set; }
+        #endregion
+    }
+}
